deb927813f828f009bfdf6c65e549ef1ddaa61d4
LYH <sot.leeyongho@gmail.com>
2024-06-30 10:35:40 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  let post = '맛집';
+  useState
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{color : 'red' , fontSize : '20px'}}>스터디임</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
